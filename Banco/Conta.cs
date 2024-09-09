e209cb86ac1c0028de39f1b80fcf0290eb22eea3
Cristian Serena <devcristian02@gmail.com>
2024-09-09 20:55:06 -0300

    Atividade Banco

---
+++ Banco/Conta.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Banco
+{
+    public class Conta
+    {
+        private int _numero;
+        private string _titular;
+        private double _saldo;
+
+        public Conta(int numero, string titular)
+        {
+            _numero = numero;
+            _titular = titular;
+        }
+
+        public Conta(int numero, string titular, double depositoInicial)
+        {
+            _numero = numero;
+            _titular = titular;
+            Depositar(depositoInicial);
+        }
+
+        public int Numero
+        {
+            get { return _numero; }
+        }
+
+        public string Titular
+        {
+            get { return _titular; }
+            set { _titular = value; }
+        }
+
+        public double Saldo
+        {
+            get { return _saldo; }
+        }
+
+        public void Depositar(double quantia)
+        {
+            _saldo += quantia;
+        }
+
+        public void Sacar(double quantia)
+        {
+            _saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                   + _numero
+                   + ", Titular: "
+                   + _titular
+                   + ", Saldo: R$ "
+                   + _saldo.ToString("F2");
+        }
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using Banco;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            Conta conta;
+
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if (resposta == 's')
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new Conta(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new Conta(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para depósito: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+            conta.Depositar(valorDeposito);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            conta.Sacar(valorSaque);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
